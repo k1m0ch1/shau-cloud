3fc76e9b32d5dfa12df1c5c6355924f1a7599ff7
k1m0ch1 <yahya.kimochi@gmail.com>
2016-08-31 21:34:00 +0700

    Beta v1.1
    tested and have a little bug

--- shau-cloud.ino
+++ shau-cloud.ino
@@ -3,25 +3,25 @@
 
 Hx711 scale(A1, A0);
 
-#define LED_PIN     8
-#define NUM_LEDS    20
+#define NUM_LEDS    114
 #define BRIGHTNESS  64
 #define LED_TYPE    WS2812B
 #define COLOR_ORDER GRB
-#define NUM_STRIPS   3
+#define NUM_STRIPS   11
 #define UPDATES_PER_SECOND 100
 
 CRGB leds[NUM_STRIPS][NUM_LEDS];
 CRGBPalette16 currentPalette;
 TBlendType    currentBlending;
-const uint8_t PIN_NO[] = { 8, 9, 11 };
 const uint8_t timernya = 1;
 unsigned long time;
 uint8_t lapse = 0;
 
-extern CRGBPalette16 langitsenja, langitbiru;
+
+extern CRGBPalette16 langitsenja, langitbiru, warnadunia;
 extern const TProgmemPalette16 langitsenja_p PROGMEM;
 extern const TProgmemPalette16 langitbiru_p PROGMEM;
+extern const TProgmemPalette16 warnadunia_p PROGMEM;
 
 const uint8_t BUKU[4][2]    = { { 7,0 }, { 7,1 }, { 7,2 }, { 7,3 } };
 const uint8_t ADALAH[7][2]  = { { 8,0 }, { 8,1 }, { 8,2 }, { 8,3 }, { 8,4 }, { 8,5 }, { 8,6 } };
@@ -29,23 +29,25 @@ const uint8_t JENDELA[7][2] = { { 0,2 }, { 1,2 }, { 2,3 }, { 3,3 }, { 4,4 }, { 5
 const uint8_t DUNIA[5][2]   = { { 9,1 }, { 7,1 }, { 5,1 }, { 3,1 }, { 1,1 } };
 const uint8_t MICLIB[7][2]  = { { 10,0 }, { 10,1 }, { 10,2 }, { 10,3 }, { 10,4 }, { 10,5 } };
 const uint8_t ALUN[4][2]    = { { 6,5 }, { 4,5 }, { 2,5 }, { 0,5 } };
-const uint8_t BANDUNG[7][2] = { { 6,1 }, { 5,1 }, { 4,2 }, { 3,2 }, { 2,3 }, { 1,3 } };
+const uint8_t BANDUNG[7][2] = { { 7,0 }, { 6,1 }, { 5,1 }, { 4, 2 }, { 3,2 }, { 2,3 }, { 1,3 } };
 const uint8_t SHAU[4][2]    = { { 4,0 }, { 5,0 }, { 6,1 }, { 7,1 } };
+
+int eeq = 0 ;
 
 void setup() {
     delay( 3000 ); // power-up safety delay
-
-    FastLED.addLeds<LED_TYPE, 8, COLOR_ORDER>(leds[0], NUM_LEDS).setCorrection( TypicalLEDStrip );
-    FastLED.addLeds<LED_TYPE, 9, COLOR_ORDER>(leds[1], NUM_LEDS).setCorrection( TypicalLEDStrip );
-    FastLED.addLeds<LED_TYPE, 11, COLOR_ORDER>(leds[2], NUM_LEDS).setCorrection( TypicalLEDStrip );
-//    FastLED.addLeds<LED_TYPE, 8, COLOR_ORDER>(leds[3], NUM_LEDS).setCorrection( TypicalLEDStrip );
-//    FastLED.addLeds<LED_TYPE, 9, COLOR_ORDER>(leds[4], NUM_LEDS).setCorrection( TypicalLEDStrip );
-//    FastLED.addLeds<LED_TYPE, 11, COLOR_ORDER>(leds[5], NUM_LEDS).setCorrection( TypicalLEDStrip );
-//    FastLED.addLeds<LED_TYPE, 8, COLOR_ORDER>(leds[6], NUM_LEDS).setCorrection( TypicalLEDStrip );
-//    FastLED.addLeds<LED_TYPE, 9, COLOR_ORDER>(leds[7], NUM_LEDS).setCorrection( TypicalLEDStrip );
-//    FastLED.addLeds<LED_TYPE, 11, COLOR_ORDER>(leds[8], NUM_LEDS).setCorrection( TypicalLEDStrip );
-//    FastLED.addLeds<LED_TYPE, 8, COLOR_ORDER>(leds[9], NUM_LEDS).setCorrection( TypicalLEDStrip );
-//    FastLED.addLeds<LED_TYPE, 9, COLOR_ORDER>(leds[10], NUM_LEDS).setCorrection( TypicalLEDStrip );
+  
+    FastLED.addLeds<LED_TYPE, 2, COLOR_ORDER>(leds[10], NUM_LEDS).setCorrection( TypicalLEDStrip );
+    FastLED.addLeds<LED_TYPE, 3, COLOR_ORDER>(leds[9], NUM_LEDS).setCorrection( TypicalLEDStrip );
+    FastLED.addLeds<LED_TYPE, 4, COLOR_ORDER>(leds[8], NUM_LEDS).setCorrection( TypicalLEDStrip );
+    FastLED.addLeds<LED_TYPE, 5, COLOR_ORDER>(leds[7], NUM_LEDS).setCorrection( TypicalLEDStrip );
+    FastLED.addLeds<LED_TYPE, 6, COLOR_ORDER>(leds[6], NUM_LEDS).setCorrection( TypicalLEDStrip );
+    FastLED.addLeds<LED_TYPE, 7, COLOR_ORDER>(leds[5], NUM_LEDS).setCorrection( TypicalLEDStrip );
+    FastLED.addLeds<LED_TYPE, 8, COLOR_ORDER>(leds[4], NUM_LEDS).setCorrection( TypicalLEDStrip );
+    FastLED.addLeds<LED_TYPE, 9, COLOR_ORDER>(leds[3], NUM_LEDS).setCorrection( TypicalLEDStrip );
+    FastLED.addLeds<LED_TYPE, 10, COLOR_ORDER>(leds[2], NUM_LEDS).setCorrection( TypicalLEDStrip );
+    FastLED.addLeds<LED_TYPE, 11, COLOR_ORDER>(leds[1], NUM_LEDS).setCorrection( TypicalLEDStrip );
+    FastLED.addLeds<LED_TYPE, 12, COLOR_ORDER>(leds[0], NUM_LEDS).setCorrection( TypicalLEDStrip );
 
     FastLED.setBrightness(BRIGHTNESS);    
     Serial.begin(9600);
@@ -54,28 +56,33 @@ void setup() {
 
 void loop(){
 
-    time = millis();
-    
+   
     static uint8_t startIndex = 0;
     startIndex = startIndex + 1;
 
-    Serial.println(time);
-    
-    lapse = time%3600? +lapse:lapse;
+    time = millis();
 
-    if(lapse>10){
-      awan(langitsenja_p, startIndex);
+    lapse = time%10000?lapse+1:lapse;
+
+    if(time<13000){
+       awan(langitbiru_p, startIndex);
     }else{
-      awan(langitbiru_p, startIndex);
+      awan(warnadunia_p, startIndex);
+     // crosswords(500,500,100);
     }
 
-    if(scale.getGram()>3){
-      crosswords(5000, 3000, 2000);
+    if(eeq==60){
+      Serial.print(scale.getGram(), 1);
+      Serial.println(" g");
+      if(scale.getGram()>100  || scale.getGram()<-100 ){
+        crosswords(500,500,100);
+      }
+      eeq=0;
     }
-    //crosswords(2000, 5000, 1000);
-    
+
     FastLED.show();
-    FastLED.delay(15000 / UPDATES_PER_SECOND);
+    FastLED.delay(500 / UPDATES_PER_SECOND);
+    eeq++;
 }
 
 void awan( CRGBPalette16 warna, uint8_t colorIndex ){
@@ -91,67 +98,180 @@ void awan( CRGBPalette16 warna, uint8_t colorIndex ){
     }
 }
 
-// leds[10] = { "M", "I", "C", "L", "I", "B" }
-// leds[9]  = { "O", "D", "I", "N", "E" }
-// leds[8]  = { "A", "D", "A", "L", "A", "H" }
-// leds[7]  = { "B", "U", "K", "U", "Q" }
-// leds[6]  = { "C", "A", "L", "Y", "D", "A" }
-// leds[5]  = { "H", "N", "E", "E", "L" }
-// leds[4]  = { "S", "L", "D", "S", "E", "L" }
-// leds[3]  = { "I", "I", "U", "D", "R" }
-// leds[2]  = { "X", "U", "V", "N", "E", "U" }
-// leds[1]  = { "M", "A", "E", "G", "Z"" }
-// leds[0]  = { "I", "N", "J", "S", "U", "N" }
-
-//sequence
-// 1. BUKU
-// leds[7][0-3]
-// 2. ADALAH
-// leds[8][0-6]
-// 3. JENDELA
-// leds[0][2], leds[1][2], leds[2][3], leds[3][3], leds[4][4], leds[5][4], leds[6][5]
-// 4. DUNIA
-// leds[9][1], leds[7][1], leds[5][1], leds[3][1], leds[1][1]
-// 5. MICLIB
-// leds[10][0-5]
-// 6. ALUN
-// leds[6][5], leds[4][5], leds[2][5], leds[0][5]
-// 7. BANDUNG
-// leds[7][0], leds[6][1], leds[5][1], leds[4][2], leds[3][2], leds[2][3], leds[1][3]
-// 8. SHAU
-// leds[4][0], leds[5][0], leds[6][1], leds[7][1]
-
-// v0.2 crosswords with fadeout effect
-
 void crosswords(int timeDelayBright, int timeDelayDark, int timeDelayPerWord){
     
     resetLed(timeDelayPerWord);
-    uint8_t brightness = 0;
+    uint8_t brightness = 255;
     uint8_t fadeAmount = 5;
     uint8_t fadeTime = 0;
     boolean notEnd = true;
 
     while(notEnd){
-      for(uint8_t x=0; x<7; x++){ leds[JENDELA[x][0]][JENDELA[x][1]].setHSV(255, 0, brightness); }
+      for(uint8_t x=0; x<4; x++){ 
+        for(uint8_t y=0; y<=76;y++){
+           leds[BUKU[x][0]][y] = CRGB::DarkRed; 
+           leds[BUKU[x][0]][y].fadeLightBy(brightness);
+        }
+       }
+       
       FastLED.show();
-      brightness = brightness +  fadeAmount;
+      brightness = brightness -  fadeAmount;
       if(brightness == 0 || brightness == 255){ fadeAmount = -fadeAmount; }   
       delay(20);
-      if(brightness == 0){ delay(timeDelayBright); fadeTime++; }
-      if(brightness == 255){ delay(timeDelayDark); fadeTime++; }
+      if(brightness == 255){ delay(timeDelayBright); fadeTime++; }
+      if(brightness == 0){ delay(timeDelayDark); fadeTime++; }
       if(fadeTime == 2 ){ notEnd = false; }
     }
 
-    resetLed(timeDelayPerWord); notEnd=true; brightness=0; fadeAmount=5; fadeTime=0;
+    resetLed(timeDelayPerWord); brightness = 255; fadeAmount = 5; fadeTime = 0; notEnd = true;
 
     while(notEnd){
-      for(uint8_t x=0; x<7; x++){ leds[JENDELA[x][0]][JENDELA[x][1]].setHSV(255, 0, brightness); }
+      for(uint8_t x=0; x<7; x++){ 
+        for(uint8_t y=0; y<=114;y++){
+           leds[ADALAH[x][0]][y] = CRGB::DarkRed; 
+           leds[ADALAH[x][0]][y].fadeLightBy(brightness);
+        }
+       }
+       
       FastLED.show();
-      brightness = brightness +  fadeAmount;
+      brightness = brightness -  fadeAmount;
       if(brightness == 0 || brightness == 255){ fadeAmount = -fadeAmount; }   
       delay(20);
-      if(brightness == 0){ delay(timeDelayBright); fadeTime++; }
-      if(brightness == 255){ delay(timeDelayDark); fadeTime++; }
+      if(brightness == 255){ delay(timeDelayBright); fadeTime++; }
+      if(brightness == 0){ delay(timeDelayDark); fadeTime++; }
+      if(fadeTime == 2 ){ notEnd = false; }
+    }
+
+    resetLed(timeDelayPerWord); brightness = 255; fadeAmount = 5; fadeTime = 0; notEnd = true;
+
+    while(notEnd){
+      for(uint8_t x=0; x<7; x++){ 
+        for(uint8_t y=(JENDELA[x][1]*19);y<((JENDELA[x][1]*19)+19); y++){
+           leds[JENDELA[x][0]][y] = CRGB::DarkRed; 
+           leds[JENDELA[x][0]][y].fadeLightBy(brightness);
+        }
+       }
+       
+      FastLED.show();
+      brightness = brightness -  fadeAmount;
+      if(brightness == 0 || brightness == 255){ fadeAmount = -fadeAmount; }   
+      delay(20);
+      if(brightness == 255){ delay(timeDelayBright); fadeTime++; }
+      if(brightness == 0){ delay(timeDelayDark); fadeTime++; }
+      if(fadeTime == 2 ){ notEnd = false; }
+    }
+
+    resetLed(timeDelayPerWord); brightness = 255; fadeAmount = 5; fadeTime = 0; notEnd = true;
+
+    while(notEnd){
+      for(uint8_t x=0; x<5; x++){ 
+        for(uint8_t y=(DUNIA[x][1]*19);y<((DUNIA[x][1]*19)+19); y++){
+           leds[DUNIA[x][0]][y] = CRGB::DarkRed; 
+           leds[DUNIA[x][0]][y].fadeLightBy(brightness);
+        }
+       }
+       
+      FastLED.show();
+      brightness = brightness -  fadeAmount;
+      if(brightness == 0 || brightness == 255){ fadeAmount = -fadeAmount; }   
+      delay(20);
+      if(brightness == 255){ delay(timeDelayBright); fadeTime++; }
+      if(brightness == 0){ delay(timeDelayDark); fadeTime++; }
+      if(fadeTime == 2 ){ notEnd = false; }
+    }
+
+    resetLed(timeDelayPerWord); brightness = 255; fadeAmount = 5; fadeTime = 0; notEnd = true;
+
+    while(notEnd){
+      for(uint8_t x=0; x<6; x++){ 
+        for(uint8_t y=(MICLIB[x][1]*19);y<((MICLIB[x][1]*19)+19); y++){
+           leds[MICLIB[x][0]][y] = CRGB::DarkRed; 
+           leds[MICLIB[x][0]][y].fadeLightBy(brightness);
+        }
+       }
+       
+      FastLED.show();
+      brightness = brightness -  fadeAmount;
+      if(brightness == 0 || brightness == 255){ fadeAmount = -fadeAmount; }   
+      delay(20);
+      if(brightness == 255){ delay(timeDelayBright); fadeTime++; }
+      if(brightness == 0){ delay(timeDelayDark); fadeTime++; }
+      if(fadeTime == 2 ){ notEnd = false; }
+    }
+
+    resetLed(timeDelayPerWord); brightness = 255; fadeAmount = 5; fadeTime = 0; notEnd = true;
+
+    while(notEnd){
+      for(uint8_t x=0; x<4; x++){ 
+        for(uint8_t y=(ALUN[x][1]*19);y<((ALUN[x][1]*19)+19); y++){
+           leds[ALUN[x][0]][y] = CRGB::DarkRed; 
+           leds[ALUN[x][0]][y].fadeLightBy(brightness);
+        }
+       }
+       
+      FastLED.show();
+      brightness = brightness -  fadeAmount;
+      if(brightness == 0 || brightness == 255){ fadeAmount = -fadeAmount; }   
+      delay(20);
+      if(brightness == 255){ delay(timeDelayBright); fadeTime++; }
+      if(brightness == 0){ delay(timeDelayDark); fadeTime++; }
+      if(fadeTime == 2 ){ notEnd = false; }
+    }
+
+    resetLed(timeDelayPerWord); brightness = 255; fadeAmount = 5; fadeTime = 0; notEnd = true;
+
+    while(notEnd){
+      for(uint8_t x=0; x<4; x++){ 
+        for(uint8_t y=(ALUN[x][1]*19);y<((ALUN[x][1]*19)+19); y++){
+           leds[ALUN[x][0]][y] = CRGB::DarkRed; 
+           leds[ALUN[x][0]][y].fadeLightBy(brightness);
+        }
+       }
+       
+      FastLED.show();
+      brightness = brightness -  fadeAmount;
+      if(brightness == 0 || brightness == 255){ fadeAmount = -fadeAmount; }   
+      delay(20);
+      if(brightness == 255){ delay(timeDelayBright); fadeTime++; }
+      if(brightness == 0){ delay(timeDelayDark); fadeTime++; }
+      if(fadeTime == 2 ){ notEnd = false; }
+    }
+
+    resetLed(timeDelayPerWord); brightness = 255; fadeAmount = 5; fadeTime = 0; notEnd = true;
+
+    while(notEnd){
+      for(uint8_t x=0; x<7; x++){ 
+        for(uint8_t y=(BANDUNG[x][1]*19);y<((BANDUNG[x][1]*19)+19); y++){
+           leds[BANDUNG[x][0]][y] = CRGB::DarkRed; 
+           leds[BANDUNG[x][0]][y].fadeLightBy(brightness);
+        }
+       }
+       
+      FastLED.show();
+      brightness = brightness -  fadeAmount;
+      if(brightness == 0 || brightness == 255){ fadeAmount = -fadeAmount; }   
+      delay(20);
+      if(brightness == 255){ delay(timeDelayBright); fadeTime++; }
+      if(brightness == 0){ delay(timeDelayDark); fadeTime++; }
+      if(fadeTime == 2 ){ notEnd = false; }
+    }
+
+    resetLed(timeDelayPerWord); brightness = 255; fadeAmount = 5; fadeTime = 0; notEnd = true;
+
+    while(notEnd){
+      for(uint8_t x=0; x<4; x++){ 
+        for(uint8_t y=(SHAU[x][1]*19);y<((SHAU[x][1]*19)+19); y++){
+           leds[SHAU[x][0]][y] = CRGB::DarkRed; 
+           leds[SHAU[x][0]][y].fadeLightBy(brightness);
+        }
+       }
+       
+      FastLED.show();
+      brightness = brightness -  fadeAmount;
+      if(brightness == 0 || brightness == 255){ fadeAmount = -fadeAmount; }   
+      delay(20);
+      if(brightness == 255){ delay(timeDelayBright); fadeTime++; }
+      if(brightness == 0){ delay(timeDelayDark); fadeTime++; }
       if(fadeTime == 2 ){ notEnd = false; }
     }
 }
@@ -180,3 +300,9 @@ const TProgmemRGBPalette16 langitbiru_p PROGMEM ={
     0x0000ff, 0x0000ff, 0xadd8e6, 0x87ceeb
 };
 
+const TProgmemRGBPalette16 warnadunia_p PROGMEM ={
+    0x0000ff, 0x0000ff, 0x00c6ff, 0x00c6ff,    
+    0xff00a2, 0xff00a2, 0x00c6ff, 0x0000ff,    
+    0x00c6ff, 0x0000ff, 0xff00a2, 0x00c6ff,    
+    0x0000ff, 0x0000ff, 0xadd8e6, 0xff00a2
+};
